4986453fa57e3609fa256d8f4a3c27262f557566
darya <idarzamasova@yandex.ru>
2025-04-28 21:07:31 +0300

    add create task

---
+++ TaskLb/Models/TaskModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskLb.Models
+{
+    internal class TaskModel
+    {
+    }
+}
--- TaskLb/obj/Debug/net8.0/TaskLb.AssemblyInfo.cs
+++ TaskLb/obj/Debug/net8.0/TaskLb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskLb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f418a460e3f3c13227dd7fdfbae95dc7ea0c490")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskLb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskLb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
