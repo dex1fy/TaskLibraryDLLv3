d486d350f90e008945602f7956dc421c9848abdf
darya <idarzamasova@yandex.ru>
2025-04-28 21:40:38 +0300

    create

--- TaskLb/Models/TaskModel.cs
+++ TaskLb/Models/TaskModel.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+
 
 namespace TaskLb.Models
 {
-    internal class TaskModel
+    public class TaskModel
     {
+        public Guid Id { get; set; } // Уникальный идентификатор задачи
+        public string Title { get; set; } // Название задачи
+        public string Description { get; set; } // Описание задачи
+       
+
+        public DateTime Deadline { get; set; }
+
+        public TaskModel(string title, string description)
+        {
+            Id = Guid.NewGuid(); // Генерируем уникальный идентификатор
+            Title = title;
+            Description = description;
+           
+        }
     }
+
 }
--- TaskLb/Services/TaskManager.cs
+++ TaskLb/Services/TaskManager.cs
@@ -1,7 +1,28 @@
-﻿namespace TaskLb
-{
-    public class Class1
-    {
+﻿using TaskLb.Models;
+using System.Collections.Generic;
 
+namespace TaskLb.Services
+{
+    public class TaskManager
+    {
+        private List<TaskModel> tasks = new List<TaskModel>();
+
+        // Добавляет новую задачу
+        public void AddTask(TaskModel task)
+        {
+            tasks.Add(task);
+        }
+
+        // Удаляет задачу по её идентификатору
+        public bool RemoveTask(Guid taskId)
+        {
+            var task = tasks.Find(t => t.Id == taskId);
+            if (task != null)
+            {
+                tasks.Remove(task);
+                return true;
+            }
+            return false;
+        }
     }
 }
